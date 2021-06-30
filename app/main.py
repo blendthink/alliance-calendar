fef96968e7ecfe4d6e1a260aa92fbaf474dd4c8c
blendthink <info@blendthink.dev>
2021-06-30 09:35:19 +0900

    カレンダー登録処理に必要なものは register_holiday_to_calendar の中へ

--- app/main.py
+++ app/main.py
@@ -1,19 +1,14 @@
-import json
 from pathlib import Path
 
-from settings import GOOGLE_EMAIL_ADDRESS
-
-from sequence import extract_date_list_of_holiday
 from sequence import download_calendar_image
+from sequence import extract_date_list_of_holiday
 from sequence import register_holiday_to_calendar
 
 FILE_PARENT_PATH = Path(__file__).parent
 DOWNLOADS_PATH = FILE_PARENT_PATH.joinpath('downloads')
-CREDENTIALS_PATH = FILE_PARENT_PATH.joinpath('credentials.json')
 
 if __name__ == '__main__':
     month = 7
     image_destination_path = download_calendar_image(month=month, downloads_path=DOWNLOADS_PATH)
     date_list = extract_date_list_of_holiday(month=month, image_destination_path=image_destination_path)
-    credentials_obj = json.load(CREDENTIALS_PATH.open())
-    register_holiday_to_calendar(credentials_obj=credentials_obj, email_address=GOOGLE_EMAIL_ADDRESS ,date_list=date_list)
+    register_holiday_to_calendar(date_list=date_list)
--- app/sequence/register_holiday_to_calendar.py
+++ app/sequence/register_holiday_to_calendar.py
@@ -1,5 +1,9 @@
 import datetime
+import json
+from pathlib import Path
 from typing import List
+
+from app import settings
 
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
@@ -7,9 +11,12 @@ from googleapiclient.discovery import build
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
-def register_holiday_to_calendar(credentials_obj: object, email_address: str, date_list: List[datetime.date]):
+def register_holiday_to_calendar(date_list: List[datetime.date]):
+    credentials_path = Path(__file__).parent.parent.joinpath('credentials.json')
+    credentials_obj = json.load(credentials_path.open())
+
     credentials = Credentials.from_service_account_info(credentials_obj, scopes=SCOPES)
-    delegated_credentials = credentials.with_subject(email_address)
+    delegated_credentials = credentials.with_subject(settings.GOOGLE_EMAIL_ADDRESS)
 
     service = build('calendar', 'v3', credentials=delegated_credentials)
 
